d4fcb6cb3598d2b3b9c516e169944c7086201af2
Xavier Marrugat <x.m.p@hotmail.es>
2024-02-06 17:00:42 +0100

    Update ollama.py

--- infohound/tool/ai_assistant/ollama.py
+++ infohound/tool/ai_assistant/ollama.py
@@ -1,12 +1,24 @@
-from langchain_community.llms import Ollama
-from infohound_project.settings import OLLAMA_URL, OLLAMA_MODEL
+from ollama import Client
+from infohound_project.settings import OLLAMA_URL,OLLAMA_MODEL
+
+def check_or_pull_model(client):
+    models = client.list()
+    present = False
+    for model in models["models"]:
+        if OLLAMA_MODEL == model["name"].split(":")[0]:
+            present = True
+    if not present:
+        client.pull(OLLAMA_MODEL)
 
 def ollama_flexible_prompt(in_prompt):
-    ollama = Ollama(base_url=OLLAMA_URL, model=OLLAMA_MODEL)
-
+    client = Client(host=OLLAMA_URL)
+    check_or_pull_model(client)
+    desc = None
     try:
-        res = ollama(in_prompt)
-        return res
+        res = client.generate(model=OLLAMA_MODEL,prompt=in_prompt)
     except Exception as e:
-        print(f"Error en la llamada a Ollama: {e}")
-        return None
+        print(f"Could not call Ollama instance: {e}")
+
+    if "response" in res:
+        desc = res["response"].strip()
+    return desc
