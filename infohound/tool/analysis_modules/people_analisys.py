cfb2029fe0ede25cf610d2932b91dd3ebf1df2da
Xavier Marrugat <x.m.p@hotmail.es>
2024-02-07 09:57:45 +0100

    Update people_analisys.py

--- infohound/tool/analysis_modules/people_analisys.py
+++ infohound/tool/analysis_modules/people_analisys.py
@@ -3,15 +3,15 @@ from infohound.models import People
 from infohound.tool.ai_assistant import ollama
 
 def summarize_profile(domain_id):
-    queryset = People.objects.filter(domain_id=domain_id)
-    
+    queryset = People.objects.filter(domain_id=domain_id, ocupation_summary__contains="This profile doesn't have a description yet")
+
     for entry in queryset.iterator():
         try:
-            summarize_prompt = "Summarize the ocupation of the person in just one pharagraph given the following data: "
+            summarize_prompt = "Summarize the ocupation of the person in just 150 words given the following data: "
             raw_data = entry.raw_metadata
             print ("Executing AI-Powered Profile Analisis of: " + entry.name)
             entry.ocupation_summary = ollama.ollama_flexible_prompt(summarize_prompt + raw_data)
             print ("Summary: " +entry.ocupation_summary)
             entry.save()
         except Exception as e:
-            print(f"Unexpected error: {str(e)}")
+            print(f"Error inesperado: {str(e)}")
