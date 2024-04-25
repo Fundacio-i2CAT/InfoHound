82b8d6bbc8c5f6daeacfe68040ec74c1a5b917c8
Xavier Marrugat <x.m.p@hotmail.es>
2024-04-25 13:48:32 +0200

    Update firefox_monitor.py

--- infohound/tool/data_sources/leaks/firefox_monitor.py
+++ infohound/tool/data_sources/leaks/firefox_monitor.py
@@ -9,9 +9,9 @@ def isLeaked(email):
 		client = requests.Session()
 		response = client.get(url)
 		csrf = re.findall('data-csrf-token="(\\d.+)"', response.text)
-		print(csrf)
 		if len(csrf) == 0:
 			retry -= 1
+			continue
 		client.headers.update({"x-csrf-token":csrf[0], 'Content-Type': 'application/json'})
 		res = client.post('https://monitor.firefox.com/api/v1/scan/',  json={'email': email}).json()
 		found = len(res["breaches"]) > 0
