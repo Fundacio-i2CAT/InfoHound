1e0e2248b954e2023aa553e41e755aee395ea400
Xavier Marrugat <x.m.p@hotmail.es>
2024-05-08 17:02:00 +0200

    Small improvements

--- infohound/infohound_config.sample.py
+++ infohound/infohound_config.sample.py
@@ -1,15 +1,4 @@
 # CONFIG FILE
-
-postgress_sql = {
-	'DB_NAME': 'infohound_db',
-	'DB_USER': 'postgres',
-	'DB_PASSWORD': 'postgres',
-	'DB_HOST': 'localhost',
-	'DB_PORT': '5432',
-}
-
-
-# 3rd Party Services
 SHODAN_KEY = "" #currently not used
 LEAK_LOOKUP_KEY = ""
 GOOGLE_API_KEY = ""
